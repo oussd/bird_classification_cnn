a1e195008c0a07e430ace4c0a1e5b465ed54f942
Marwa98M <mm1604391@gmail.com>
2024-04-07 11:17:36 +0300

    coping the modell

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-docker build -t django_app .
+docker build -t django_app -f django_app.dockerfile .
+
+git checkout train
+cp -r ./models/bird_classification_model.h5 ./model
+
+
 docker run -p 8000:8000 django_app
+
